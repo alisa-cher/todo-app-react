a7042874930b375e702c0a2de6376929e1f1be5f
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2020-03-26 16:38:48 +0100

    Added local state for TodoListItem component

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,30 +1,58 @@
-import React from "react";
+import React, {Component} from "react";
 import "./todo-list-item.css"
 
-const TodoListItem = (props) => {
-  const {label, important = false} = props;
-  const style = {
-    color: important ? "red" : ""
-  };
+export default class TodoListItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      done: false,
+      important: false
+    };
+    this.onLabelClick = this.onLabelClick.bind(this);
+    this.onImportantButtonClick = this.onImportantButtonClick.bind(this);
+  }
 
-  return <div style={style} className="todo-list-item">
-    <div
-      className="todo-list-item-label"
-      style={style}>
-      <span>{label}</span>
-    </div>
-    <div className="todo-list-item__button-group">
-      <button type="button"
-              className="uk-button uk-button-default todo-list-item__button">
-        <i className="fa fa-exclamation"/>
-      </button>
-      <button type="button"
-              className="uk-button uk-button-default todo-list-item__button">
-        <i className="fa fa-trash-o"/>
-      </button>
-    </div>
-  </div>;
-};
+  onLabelClick() {
+    this.setState((prevState) => {
+      return {
+        done: !prevState.done
+      }
+    })
+  }
 
-export default TodoListItem;
+  onImportantButtonClick() {
+    if (!this.state.done) {
+      this.setState((prevState) => {
+        return {
+          important: !prevState.important
+        }
+      })
+    }
+  }
+
+  render() {
+    const {label} = this.props;
+    const {done, important} = this.state;
+
+    return <div className="todo-list-item">
+      <div
+        className={`todo-list-item-label ${important ? "todo-list-item-label--important" : ""} ${done ? "todo-list-item-label--done" : ""} `}
+        onClick={this.onLabelClick}>
+        <span>{label}</span>
+      </div>
+      <div className="todo-list-item__button-group">
+        <button type="button"
+                className="uk-button uk-button-default todo-list-item__button"
+                onClick={this.onImportantButtonClick}>
+          <i className="fa fa-exclamation"/>
+        </button>
+        <button type="button"
+                className="uk-button uk-button-default todo-list-item__button">
+          <i className="fa fa-trash-o"/>
+        </button>
+      </div>
+    </div>;
+  }
+}
+
 
